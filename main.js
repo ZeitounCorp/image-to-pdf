f17ef70cbb56765285f7410a728621082848ba84
Lenny Zeitoun <30462059+ZeitounCorp@users.noreply.github.com>
2021-06-23 01:50:07 +0300

    Now is able to receive buffer || base64 as data

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 const PDFDocument = require('pdfkit');
 const sizes = require("./sizes.json")
 
-module.exports = (pages, size) => {
-    const doc = new PDFDocument({ margin: 0, size });
-	
-	for (let index = 0; index < pages.length; index++) {
-		doc.image(pages[index], 0, 0, { fit: sizes[size], align: 'center', valign: 'center' });
+module.exports = (buff, size) => {
+	const doc = new PDFDocument({ margin: 0, size });
+	doc.image(buff, 0, 0, { fit: sizes[size], align: 'center', valign: 'center' });
 
-		if (pages.length != index + 1) doc.addPage();
-	}
+	doc.end();
 
-    doc.end();
-
-    return doc
+	return doc
 };
